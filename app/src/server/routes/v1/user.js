d70296e198d94c32e0cd7eb0c7fa64e4a2fde091
bhawna127 <bhawna_pandey@softprodigy.com>
2020-03-24 20:04:49 +0530

    bank login

--- app/src/server/routes/v1/user.js
+++ app/src/server/routes/v1/user.js
@@ -7,8 +7,16 @@ const HTTPStatus = require('http-status');
 module.exports = function(app, wagner, passport) {
    app.post('/v1/admin/login', async (req, res) => {
      passport.authenticate('local', (err, user, info) => {
-       if (!user) { res.status(HTTPStatus.NOT_FOUND).json({ success: '0', message: "failure", data: info }); }
-       else { res.status(HTTPStatus.OK).json({ success: '1', message: "success", data: info }); }
+       if (user && info.user.role_id == 1) { res.status(HTTPStatus.OK).json({ success: '1', message: "success", data: info }); }
+       else { res.status(HTTPStatus.NOT_FOUND).json({ success: '0', message: "failure", data: {} });  }
      })(req, res);
-   })
+   });
+
+   app.post('/v1/bank/login', async (req, res) => {
+      passport.authenticate('local', (err, user, info) => {
+        if(user && info.user.role_id == 2){ 
+         res.status(HTTPStatus.OK).json({ success: '1', message: "success", data: info });
+        } else {  res.status(HTTPStatus.NOT_FOUND).json({ success: '0', message: "failure", data: {} }); }
+      })(req, res);
+   });
 };
